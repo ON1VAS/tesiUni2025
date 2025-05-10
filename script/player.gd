36d38f898f674d8e9e64d411ea7ecf18ae3a8ab5
Snorry0 <96115287+Snorry0@users.noreply.github.com>
2025-05-10 21:05:54 +0200

    sistemata hitbox spada

--- script/player.gd
+++ script/player.gd
@@ -1,10 +1,29 @@
 extends CharacterBody2D
 
+@onready var sword_hitbox = $SwordHitbox/CollisionShape2D
+@onready var hitbox_timer = $HitboxTimer
+
 const GRAVITY = 400.0
 const JUMP_FORCE = -200
 @export var speed = 200
 var facing_direction = 1  # 1 = destra, -1 = sinistra
 
+# Configurazione hitbox per ogni animazione perchè se cambio gli sprite urlo, accomodiamo per i prossimi attacchi anche
+var attack_properties = {
+	"attack": {"delay": 0.1, "duration": 0.15, "keyframes": [1,2]},
+	"attackfermo": {"delay": 0.1, "duration": 0.15, "keyframes": [1,2]},
+	"attack2": {"delay": 0.15, "duration": 0.2, "keyframes": [2,3]},
+	"attackfermo2": {"delay": 0.15, "duration": 0.2, "keyframes": [2,3]}
+}
+
+func _ready():
+	sword_hitbox.disabled = true #disattivato di default
+	hitbox_timer.timeout.connect(_on_hitbox_timer_timeout)
+	
+func start_attack(anim_name: String):
+	$AnimatedSprite2D.play(anim_name)
+	var props = attack_properties[anim_name]
+	hitbox_timer.start(props.delay) #si inizia ad attivarlo per la prima volta
 func _physics_process(delta):
 	# Movimento e gravità
 	velocity.y += delta * GRAVITY
@@ -24,15 +43,13 @@ func _physics_process(delta):
 		velocity.y = JUMP_FORCE
 	
 	# Attacchi (solo se non sta già eseguendo un'animazione di attacco)
-	if $AnimatedSprite2D.animation not in ["attack", "attack2"]:
+	if $AnimatedSprite2D.animation not in attack_properties.keys():
 		if Input.is_action_just_pressed("attackfast"):
-			$AnimatedSprite2D.play("attack")
+			start_attack("attack" if velocity.x == 0 else "attackfermo")
 		elif Input.is_action_just_pressed("attackpesant"):
-			$AnimatedSprite2D.play("attack2")
-	
+			start_attack("attack2" if velocity.x == 0 else "attackfermo2")
 	# Animazioni normali (solo se non sta attaccando)
-	if $AnimatedSprite2D.animation not in ["attack", "attack2"]:
-		if not is_on_floor():
+		elif not is_on_floor():
 			$AnimatedSprite2D.play("jump" if velocity.y < 0 else "fall")
 		elif velocity.x != 0:
 			$AnimatedSprite2D.play("run")
@@ -41,10 +58,31 @@ func _physics_process(delta):
 	
 	# Flip
 	$AnimatedSprite2D.flip_h = facing_direction < 0
+	$SwordHitbox/CollisionShape2D.x = -1
 	
 	move_and_slide()
 
 
 func _on_animated_sprite_2d_animation_finished() -> void:
-	if $AnimatedSprite2D.animation in ["attack", "attack2"]:
+	if $AnimatedSprite2D.animation in attack_properties.keys():
+		sword_hitbox.disabled = true #Non mi serve più tenerla attiva
 		$AnimatedSprite2D.play("idle")
+
+
+func _on_hitbox_timer_timeout() -> void:
+	var current_anim = $AnimatedSprite2D.animation
+	
+	if current_anim in attack_properties:
+		var props = attack_properties[current_anim]
+		
+		if sword_hitbox.disabled:
+			# Attiva hitbox
+			sword_hitbox.disabled = false
+			hitbox_timer.start(props.duration)  # Durata hitbox
+		else:
+			# Disattiva hitbox
+			sword_hitbox.disabled = true
+			
+			# Se ci sono più keyframe, programma la prossima attivazione
+			var next_activation = props.delay * 1.5  # Regola questo valore
+			hitbox_timer.start(next_activation)
