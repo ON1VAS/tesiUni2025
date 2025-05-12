51ddc450a09ead2ac900c4c7502b5186f2cc5c70
Snorry0 <96115287+Snorry0@users.noreply.github.com>
2025-05-12 19:50:40 +0200

    sistemata hitbox ape piu morte

--- script/ape.gd
+++ script/ape.gd
@@ -4,18 +4,26 @@ var movement_speed = 100
 @onready var player = get_tree().get_first_node_in_group("giocatore") 
 @onready var anim = $AnimatedSprite2D
 @onready var hp = 20
-@onready var min_distance = 300
+@onready var min_distance = 30
 @onready var hurtbox = $Hurtbox
 @onready var sword_hitbox = $Pungiglione/CollisionShape2D
 @onready var hitbox_timer = $HitboxTimer
+
+var damage = 10
 
 func _ready():
 	print(self.name)
 	sword_hitbox.disabled = true #disattivato di default
 	$Pungiglione.set_collision_layer_value(2, true)  # Abilita layer 2 (player_weapon)
 	$Pungiglione.set_collision_mask_value(6, true) #abilita la maschera per colpire i nemici
+	hurtbox.set_collision_layer_value(6, true)
+	hurtbox.set_collision_mask_value(2, true)
 	if not hitbox_timer.timeout.is_connected(_on_hitbox_timer_timeout):
 		hitbox_timer.timeout.connect(_on_hitbox_timer_timeout)
+	if $Hurtbox.area_entered.is_connected(_on_hurtbox_area_entered):
+		$Hurtbox.area_entered.disconnect(_on_hurtbox_area_entered)
+	$Hurtbox.area_entered.connect(_on_hurtbox_area_entered)
+	
 
 
 # Configurazione hitbox per ogni animazione perchè se cambio gli sprite urlo, accomodiamo per i prossimi attacchi anche
@@ -29,6 +37,17 @@ func _on_animated_sprite_2d_animation_finished() -> void:
 		sword_hitbox.disabled = true #Non mi serve più tenerla attiva
 		$AnimatedSprite2D.play("idle")
 
+func take_damage(amount: int):
+	hp -= amount
+	anim.play("hurt")
+	print("Ape colpita! Vita rimanente: ", hp)
+	
+	if hp <= 0:
+		self.collision_layer = false
+		anim.play("death")
+		set_physics_process(false)
+		await anim.animation_finished
+		self.queue_free()
 
 func _on_hitbox_timer_timeout() -> void:
 	var current_anim = $AnimatedSprite2D.animation
@@ -47,3 +66,9 @@ func _on_hitbox_timer_timeout() -> void:
 			# Se ci sono più keyframe, programma la prossima attivazione
 			var next_activation = props.delay * 1.5  # Regola questo valore
 			hitbox_timer.start(next_activation)
+
+
+func _on_hurtbox_area_entered(area: Area2D) -> void:
+	if area.is_in_group("player_weapon"):
+		anim.play("attack")
+		take_damage(10)
