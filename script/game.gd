c41f63cb60bb1f4197f6f89253bf9c0a8f34e217
ON1VAS <savi.daniele01@gmail.com>
2025-05-21 15:40:49 +0200

    inizio sviluppo gameplay

--- script/game.gd
+++ script/game.gd
@@ -3,24 +3,37 @@ extends Node2D
 @onready var player = $protagonista
 @onready var player_an_sp = $protagonista/AnimatedSprite2D
 @onready var scudo = $Area2Dscudo
+@onready var dialogue_box = $DialogueBox #per le interazioni e i dialoghi
 var shader_material = ShaderMaterial.new()
+var can_start = true
 
 func _ready():
 	var shader = preload("res://scene/player.gdshader")
 	shader_material.shader = shader
 	player_an_sp.material = null #di default è spenta
-	scudo.monitoring = false
-	await get_tree().create_timer(0.1).timeout
-	scudo.monitoring = true
+	
+	
 	
 
 func _on_area_2_dscudo_body_exited(body: Node2D) -> void:
 	if body.name == "protagonista":
-		print("uscito")
+		can_start = false
+		print("uscito ", can_start)
+		dialogue_box.visible = false
 		player_an_sp.material = null
 
 
 func _on_area_2_dscudo_body_entered(body: Node2D) -> void:
 	if body.name == "protagonista":
-		print("entrato")
+		
+		can_start = true
+		print("entrato ", can_start)
+		dialogue_box.visible = true
+		dialogue_box.talk_prompt("Premi [E] per disattivare lo scudo e iniziare la difesa")
 		player_an_sp.material = shader_material
+
+func _input(event: InputEvent) -> void:
+	if can_start and event.is_action_pressed("ui_accept"):
+		$Area2Dscudo/scudo/AnimationPlayer.play("dissolvenza")
+		_on_area_2_dscudo_body_exited(player)
+		$Area2Dscudo.monitoring = false
