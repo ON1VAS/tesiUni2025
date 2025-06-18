0529774b06ef28c74cc31df1108d4eff387652e5
Snorry0 <96115287+Snorry0@users.noreply.github.com>
2025-06-18 19:38:38 +0200

    aggiunto autorun script debuff

---
+++ DebuffManager.gd
@@ -0,0 +1,35 @@
+extends Node
+
+# Parametri configurabili
+const ENERGY_THRESHOLDS = {
+	"test": 100,
+	"blocco1": 70,
+	"blocco2": 40,
+	"blocco3": 30,
+	"blocco4": 20,
+}
+
+func apply_to_player(player):
+	var energia = GlobalStats.energia
+	
+	# Reset valori
+	player.speed = 200
+	
+	# Debuff 1: rallenta
+	if energia < ENERGY_THRESHOLDS["blocco1"]:
+		player.speed = 150
+	
+	if energia < ENERGY_THRESHOLDS["blocco2"]:
+		player.speed = 120
+	
+	# Debuff 3: disabilita salto
+	if energia < ENERGY_THRESHOLDS["blocco3"]:
+		player.can_jump = false  # aggiungi una variabile nel player
+	else:
+		player.can_jump = true
+
+func enemy_damage_multiplier():
+	var energia = GlobalStats.energia
+	if energia < ENERGY_THRESHOLDS["blocco4"]:
+		return 1.5
+	return 1.0
