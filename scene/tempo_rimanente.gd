74c9b85910fb4124948ad09084e11496eac92fbe
ON1VAS <savi.daniele01@gmail.com>
2025-07-28 15:44:02 +0200

    fatte alcune modifiche al pomodoro

--- scene/tempo_rimanente.gd
+++ scene/tempo_rimanente.gd
@@ -1,6 +1,5 @@
 extends Control
 
-@onready var ore = $VBoxContainer/HBoxContainer/ore
 @onready var minuti = $VBoxContainer/HBoxContainer/minuti
 @onready var secondi = $VBoxContainer/HBoxContainer/secondi
 
@@ -12,23 +11,15 @@ func _ready():
 
 func calcola_tempo():
 	var secondi_totali = GlobalStats.secondi_totali
-	var ore_rimanenti: int = secondi_totali / 3600
 	var minuti_rimanenti: int = (secondi_totali % 3600) / 60
 	var secondi_rimanenti: int = secondi_totali % 60
 	
-	# Mostra solo ore se presenti
-	if ore_rimanenti > 0:
-		ore.text = str(ore_rimanenti)
-		minuti.text = str(minuti_rimanenti).pad_zeros(2)
-		secondi.text = str(secondi_rimanenti).pad_zeros(2)
-	# Altrimenti mostra solo minuti e secondi
-	elif minuti_rimanenti > 0:
-		ore.text = "00"
+	
+	if minuti_rimanenti > 0:
 		minuti.text = str(minuti_rimanenti)
 		secondi.text = str(secondi_rimanenti).pad_zeros(2)
 	# Altrimenti mostra solo secondi
 	else:
-		ore.text = "00"
 		minuti.text = "00"
 		secondi.text = str(secondi_rimanenti)
 	
--- script/pomodoro.gd
+++ script/pomodoro.gd
@@ -1,14 +1,43 @@
 extends Node2D
 
 
-
+#oggetti
 @onready var player = $protagonista
 @onready var player_an_sp = $protagonista/AnimatedSprite2D
-var shader_material = ShaderMaterial.new()
+@onready var timer = $HUD/timer
+@onready var timerlabel = $HUD/timerlabel
 
+#variabili
+var shader_material = ShaderMaterial.new()
+var time_left: float
 
 func _ready():
 	#disattiva la shader luminosa attorno al player
 	var shader = preload("res://scene/player.gdshader")
 	shader_material.shader = shader
-	player_an_sp.material = null #di default è spenta
+	player_an_sp.material = null #di default è spenta, dovrebbe almeno
+	
+	#setup timer
+	time_left = GlobalStats.secondi_totali
+	timerlabel.text = format_time(time_left)
+	timer.wait_time = 1.0
+	timer.timeout.connect(_on_timer_timeout)
+	timer.start()
+
+
+#gestione timer, gestisce il label e il timer
+func _on_timer_timeout():
+	time_left -= 1
+	if time_left <= 0:
+		timer.stop()
+		time_left = 0
+		# Eventuale azione al termine del timer
+		timerlabel.text = "Tempo scaduto!"
+	else:
+		timerlabel.text = format_time(time_left)
+
+#fornisce i secondi in minuti : secondi
+func format_time(seconds:float) -> String:
+	var minutes = int(seconds) / 60
+	var secs = int(seconds) % 60
+	return "%02d:%02d" % [minutes, secs]
