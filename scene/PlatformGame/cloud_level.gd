03dee5ff34f1c0422b6c84f8950fa4a0052f20fe
Snorry0 <96115287+Snorry0@users.noreply.github.com>
2025-07-29 19:30:33 +0200

    introdotta scena cloudLevel

---
+++ scene/PlatformGame/cloud_level.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+@onready var player = $protagonista
+@onready var sprite2D5 = $Sprite2D5
+@onready var sprite2D3 = $Sprite2D3
+@onready var sprite2D4 = $Sprite2D4
+
+func _process(delta):
+	# Mantieni solo la coordinata X del player, Y rimane fissa
+	sprite2D5.position.x = player.position.x
+	sprite2D4.position.x = player.position.x
+	sprite2D3.position.x = player.position.x
